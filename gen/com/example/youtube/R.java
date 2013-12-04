bc9fa866bae44026cde6ce83f65ab576e3b424d4
unknown <potlurim@686RYR1.AAUW.org>
2013-12-04 13:00:19 -0500

    Button icon and title changes
    Signed-off-by: unknown <potlurim@686RYR1.AAUW.org>

--- gen/com/example/youtube/R.java
+++ gen/com/example/youtube/R.java
@@ -27,23 +27,30 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int dice=0x7f020000;
-        public static final int dice1=0x7f020001;
-        public static final int dice2=0x7f020002;
-        public static final int dice3=0x7f020003;
-        public static final int dice4=0x7f020004;
-        public static final int dice5=0x7f020005;
-        public static final int dice6=0x7f020006;
-        public static final int dice7=0x7f020007;
-        public static final int film=0x7f020008;
-        public static final int film1=0x7f020009;
-        public static final int ic_action_info=0x7f02000a;
-        public static final int ic_action_photo=0x7f02000b;
-        public static final int ic_launcher=0x7f02000c;
-        public static final int image1=0x7f02000d;
-        public static final int image2=0x7f02000e;
-        public static final int thumb1=0x7f02000f;
-        public static final int thumb2=0x7f020010;
+        public static final int button=0x7f020000;
+        public static final int button2=0x7f020001;
+        public static final int dice=0x7f020002;
+        public static final int dice1=0x7f020003;
+        public static final int dice2=0x7f020004;
+        public static final int dice3=0x7f020005;
+        public static final int dice4=0x7f020006;
+        public static final int dice5=0x7f020007;
+        public static final int dice6=0x7f020008;
+        public static final int dice7=0x7f020009;
+        public static final int film=0x7f02000a;
+        public static final int film1=0x7f02000b;
+        public static final int ic_action_info=0x7f02000c;
+        public static final int ic_action_photo=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
+        public static final int icon1=0x7f02000f;
+        public static final int icon2=0x7f020010;
+        public static final int icon3=0x7f020011;
+        public static final int icon4=0x7f020012;
+        public static final int icon5=0x7f020013;
+        public static final int image1=0x7f020014;
+        public static final int image2=0x7f020015;
+        public static final int thumb1=0x7f020016;
+        public static final int thumb2=0x7f020017;
     }
     public static final class id {
         public static final int action_flip=0x7f060001;
