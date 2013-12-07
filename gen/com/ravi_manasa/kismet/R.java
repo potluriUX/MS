f41836ee0a983fc8e17b6060b36fbe21a55d80b2
ravitejapotluri <spoorthy.devineni@gmail.com>
2013-12-07 10:08:20 -0500

    test commit

--- gen/com/ravi_manasa/kismet/R.java
+++ gen/com/ravi_manasa/kismet/R.java
@@ -37,20 +37,22 @@ public final class R {
         public static final int dice5=0x7f020007;
         public static final int dice6=0x7f020008;
         public static final int dice7=0x7f020009;
-        public static final int film=0x7f02000a;
-        public static final int film1=0x7f02000b;
-        public static final int ic_action_info=0x7f02000c;
-        public static final int ic_action_photo=0x7f02000d;
-        public static final int ic_launcher=0x7f02000e;
-        public static final int icon1=0x7f02000f;
-        public static final int icon2=0x7f020010;
-        public static final int icon3=0x7f020011;
-        public static final int icon4=0x7f020012;
-        public static final int icon5=0x7f020013;
-        public static final int image1=0x7f020014;
-        public static final int image2=0x7f020015;
-        public static final int thumb1=0x7f020016;
-        public static final int thumb2=0x7f020017;
+        public static final int dice8=0x7f02000a;
+        public static final int dice9=0x7f02000b;
+        public static final int film=0x7f02000c;
+        public static final int film1=0x7f02000d;
+        public static final int ic_action_info=0x7f02000e;
+        public static final int ic_action_photo=0x7f02000f;
+        public static final int ic_launcher=0x7f020010;
+        public static final int icon1=0x7f020011;
+        public static final int icon2=0x7f020012;
+        public static final int icon3=0x7f020013;
+        public static final int icon4=0x7f020014;
+        public static final int icon5=0x7f020015;
+        public static final int image1=0x7f020016;
+        public static final int image2=0x7f020017;
+        public static final int thumb1=0x7f020018;
+        public static final int thumb2=0x7f020019;
     }
     public static final class id {
         public static final int action_flip=0x7f060001;
