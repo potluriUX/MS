3b5a1820cdeacc0eabdfaa4af25874b41eeb1093
ravitejapotluri <spoorthy.devineni@gmail.com>
2013-12-12 13:56:37 -0500

    deleted dice images
    i will commit the new apk to play store after going home. it needs same
    key as where i uploaded the first apk.

--- gen/com/ravi_manasa/kismet/R.java
+++ gen/com/ravi_manasa/kismet/R.java
@@ -28,36 +28,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int button=0x7f020000;
-        public static final int button2=0x7f020001;
-        public static final int dice=0x7f020002;
-        public static final int dice1=0x7f020003;
-        public static final int dice2=0x7f020004;
-        public static final int dice3=0x7f020005;
-        public static final int dice4=0x7f020006;
-        public static final int dice5=0x7f020007;
-        public static final int dice6=0x7f020008;
-        public static final int dice7=0x7f020009;
-        public static final int dice8=0x7f02000a;
-        public static final int dice9=0x7f02000b;
-        public static final int film=0x7f02000c;
-        public static final int film1=0x7f02000d;
-        public static final int ic_action_info=0x7f02000e;
-        public static final int ic_action_photo=0x7f02000f;
-        public static final int ic_launcher=0x7f020010;
-        public static final int icon1=0x7f020011;
-        public static final int icon10=0x7f020012;
-        public static final int icon2=0x7f020013;
-        public static final int icon3=0x7f020014;
-        public static final int icon4=0x7f020015;
-        public static final int icon5=0x7f020016;
-        public static final int icon6=0x7f020017;
-        public static final int icon7=0x7f020018;
-        public static final int icon8=0x7f020019;
-        public static final int icon9=0x7f02001a;
-        public static final int image1=0x7f02001b;
-        public static final int image2=0x7f02001c;
-        public static final int thumb1=0x7f02001d;
-        public static final int thumb2=0x7f02001e;
+        public static final int dice1=0x7f020001;
+        public static final int ic_action_info=0x7f020002;
+        public static final int ic_action_photo=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int icon1=0x7f020005;
+        public static final int icon18=0x7f020006;
+        public static final int icon9=0x7f020007;
+        public static final int image1=0x7f020008;
+        public static final int image2=0x7f020009;
+        public static final int micon=0x7f02000a;
+        public static final int thumb1=0x7f02000b;
+        public static final int thumb2=0x7f02000c;
     }
     public static final class id {
         public static final int action_flip=0x7f060001;
